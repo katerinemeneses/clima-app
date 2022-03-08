05ec55d1a2d79a0b5a2670bfce6570a030c72ae8
karla <parrakarla656gmail.com>
2022-03-07 19:12:13 -0500

    clima-app-terminado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import React, {useEffect,useState} from "react";
 import './App.css';
+import axios from 'axios';
+import Package from '../package.json';
+
+ 
 
 function App() {
+const [change,setchange] =  useState (true);
+const [climate,changeclimate] = useState({})
+const [temperature,assigntemperature] =useState(0)
+  const success = pos => {
+    console.log(pos.coords.latitude);
+    const latitude = pos.coords.latitude;
+    const longitude = pos.coords.longitude;
+
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=eb6f2d9e00b06c30f2d865f3e68d9f08`)
+    .then(res =>  {
+      console.log(res.data);
+      changeclimate(res.data); 
+      assigntemperature((res.data.main?.temp-(273.15)).toFixed(2));
+
+    } 
+  )
+  }
+  const convert = () =>{
+    if(change){
+      const far = (climate.main?.temp-273.15)*(9/5) + 32;
+      assigntemperature(far.toFixed(2))
+      setchange(false)
+
+    }else{
+      const degrees = (climate.main?.temp)-(273.15)
+      assigntemperature(degrees.toFixed(2))
+      setchange(true)
+    }
+  }
+
+  useEffect(()=>{
+    navigator.geolocation.getCurrentPosition(success);
+    console.log(climate);
+},[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       
+      <a className="humidity">{climate.main?.humidity}</a>
+      <h1 className="temp_max">{climate.main?.temp_max}</h1>
+      <p className="name">{climate.name}</p>
+      <a className="climate">{climate.sys?.country}</a>
+      <a className="temperature">{temperature}</a>
+      <div className="button">
+        <button onClick={convert}>converti</button>
+      </div>
+      <img className='icon' src={climate.weather?.[0].icon? `http://openweathermap.org/img/wn/${climate.weather?.[0].icon}@2x.png` : ""} alt="" />
     </div>
   );
 }
